5983f7db2f7e0d6f9f92228092785b01820e478f
RAI015 <i.scryed72342019@gmail.com>
2020-06-22 18:51:34 +0900

    文字列の扱い

--- lesson.py
+++ lesson.py
@@ -1,5 +1,30 @@
-a = 'test'
-b = a
-c = b
+print('hello')
+print("I don't know.")
+print('I don\'t know.')
+print('say "I don\'t know."')
+print("say \"I don't know.\"")
 
-print(c)
+print('hello. How are you?')
+print('hello. \nHow are you?')
+print(r'C:\name\name')
+
+print('############')
+print("""\
+line1
+line2
+line3\
+""")
+print('############')
+
+print('Hi.' * 3 + 'Mike.')
+
+print('Py' + 'thon')
+print('Py''thon')
+
+prefix = 'Py'
+print(prefix + 'thon')
+
+s = ('aaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbb')
+
+print(s)
