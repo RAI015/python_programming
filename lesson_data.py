9ba57b6dac4193b9ad24c087f89f822d6918e61b
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 00:16:30 +0900

    ディクショナリの使い所

--- lesson_data.py
+++ lesson_data.py
@@ -1,11 +1,13 @@
-x = {'a': 1}
-y = x
-y['a'] = 1000
-print(x)
-print(y)
+l = [
+    ['apple', 100],
+    ['banana', 200],
+    ['orange', 300],
+]
 
-x = {'a': 2}
-y = x.copy()
-y['a'] = 1000
-print(x)
-print(y)
+fruits = {
+    'apple': 100,
+    'banana': 200,
+    'orange': 300
+}
+
+print(fruits['apple'])
