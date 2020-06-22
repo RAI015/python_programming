95c0e780c08275fdd009e4e5bb628ca739d295d1
RAI015 <i.scryed72342019@gmail.com>
2020-06-22 21:03:36 +0900

    文字列のインデックスとスライス

--- lesson.py
+++ lesson.py
@@ -28,3 +28,20 @@ s = ('aaaaaaaaaaaaaaaaaaaaa'
      'bbbbbbbbbbbbbbbbbbbbb')
 
 print(s)
+
+word = 'Python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print('############')
+print(word[0:2])
+print(word[:2])
+print('############')
+print(word[2:])
+word = 'j' + word[1:]
+print(word[:])
+
+n = len(word)
+print(n)
