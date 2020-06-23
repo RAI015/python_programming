0cb16a79bfdc0c4df71e78c8e4ddb82f16a149f2
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 13:44:17 +0900

    Pythonのコメントについて

--- lesson_data.py
+++ lesson_data.py
@@ -1,7 +1,14 @@
-my_friends = {'A', 'C', 'D'}
-A_friends = {'B', 'D', 'E', 'F'}
-print(my_friends & A_friends) # 共通の友達
 
-f = ['apple', 'banana', 'apple', 'banana']
-kind = set(f) # 重複してるものを集める list(配列) -> set(集合)
-print(kind)
+print('XXXXX')
+
+"""
+test
+test
+test
+
+"""
+# test
+print('XXXXX')
+
+# Apple price
+some_value = 100
