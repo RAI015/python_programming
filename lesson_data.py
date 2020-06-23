711af2a0d4ced156e1c070eaddef18b51ba17480
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 23:25:40 +0900

    独自例外の作成

--- lesson_data.py
+++ lesson_data.py
@@ -1,16 +1,16 @@
-l = [1, 2, 3]
-i = 5
+class UppdercaseError(Exception):
+    pass
 
-# del l
+
+def check():
+    words = ['APPLE', 'orange', 'banana']
+    for word in words:
+        if word.isupper():
+            raise UppdercaseError(word)
+
+
 try:
-    l[0]
-except IndexError as ex:
-    print("Don't worry: {}".format(ex))
-except NameError as ex:
-    print(ex)
-except Exception as ex:
-    print('other:{}'.format(ex))
-else:
-    print('done')
-finally:
-    print('clean up')
+    check()
+except UppdercaseError as ex:
+    print('This is my fault. Go next')
+
