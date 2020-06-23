7dd494905c47e8efacdc32aa11ed4e0375e4a565
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 16:29:45 +0900

    関数定義

--- lesson_data.py
+++ lesson_data.py
@@ -1,6 +1,27 @@
-d = {'x': 100, 'y': 200}
+def say_something():
+    s = 'hi'
+    return s
 
-# print(d.items())
 
-for k, v in d.items():
-    print(k, ':', v)
+# print(type(say_something))
+#
+# f = say_something
+# f()
+
+result = say_something()
+print(result)
+
+
+def what_is_this(color):
+    if color == 'red':
+        return 'tomato'
+    elif color == 'green':
+        return 'green pepper'
+    else:
+        return "I don't know"
+
+
+result = what_is_this('red')
+result = what_is_this('green')
+result = what_is_this('yellow')
+print(result)
