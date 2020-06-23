aecb535e73297f01093cb4676291ec02b030e2e4
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 15:11:17 +0900

    for文とbreak, continue

--- lesson_data.py
+++ lesson_data.py
@@ -1,6 +1,22 @@
-while True:
-    word = input('Enter:')
-    num = int(word)
-    if num == 100:
+# some_list = [1, 2, 3, 4, 5]
+
+# i = 0
+# while i < len(some_list):
+#     print(some_list[i])
+#     i += 1
+
+# for i in some_list:
+#     print(i)
+
+# for s in 'abcde':
+#     print(s)
+
+for word in ['My', 'name', 'is', 'Mike']:
+    if word == 'name':
         break
-    print('next')
+    print(word)
+
+for word in ['My', 'name', 'is', 'Mike']:
+    if word == 'name':
+        continue
+    print(word)
