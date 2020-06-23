5b06e9058559e225297833ea4d1a87d4d4c6cf9a
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 16:44:42 +0900

    デフォルト引数で気をつけること

--- lesson_data.py
+++ lesson_data.py
@@ -1,9 +1,34 @@
-def menu(entree='beef', drink='wine', dessert='ice'):
-    print('entree = ', entree)
-    print('drink = ', drink)
-    print('dessert = ', dessert)
+def test_func(x, l=[]):
+    l.append(x)
+    return l
 
 
-menu(entree='chicken', dessert='ice', drink='beer')
-menu()
+# y = [1, 2, 3]
+# r = test_func(100, y)
+# print(r)
+#
+# y = [1, 2, 3]
+# r = test_func(200, y)
+# print(r)
 
+r = test_func(100)
+print(r)
+
+# リストは参照渡しなので意図しない動きをする
+# 暗黙のルールで、デフォルト引数にリストやディクショナリをするなというのがある
+r = test_func(100)
+print(r)
+
+
+# 通常はこういう書き方をする方が好ましい
+def test_func2(x, l=None):
+    if l is None:
+        l = []
+    l.append(x)
+    return l
+
+r = test_func2(100)
+print(r)
+
+r = test_func2(100)
+print(r)
