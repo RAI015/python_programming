c0a141946a1d5afdb8a85666338e30ba96f4fe32
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 16:32:17 +0900

    関数の引数と返り値の宣言

--- lesson_data.py
+++ lesson_data.py
@@ -1,27 +1,5 @@
-def say_something():
-    s = 'hi'
-    return s
+def add_num(a: int, b: int) -> int:
+    return a + b
 
-
-# print(type(say_something))
-#
-# f = say_something
-# f()
-
-result = say_something()
-print(result)
-
-
-def what_is_this(color):
-    if color == 'red':
-        return 'tomato'
-    elif color == 'green':
-        return 'green pepper'
-    else:
-        return "I don't know"
-
-
-result = what_is_this('red')
-result = what_is_this('green')
-result = what_is_this('yellow')
-print(result)
+r = add_num('a', 'b')
+print(r)
