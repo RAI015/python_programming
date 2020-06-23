59f1c8cbff13c4ae08052e1919ef27bae4033f76
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 16:48:57 +0900

    位置引数のタプル化

--- lesson_data.py
+++ lesson_data.py
@@ -1,34 +1,9 @@
-def test_func(x, l=[]):
-    l.append(x)
-    return l
+def say_something(word, *args):
+    print('word = ', word)
+    for arg in args:
+        print(arg)
 
+say_something('Hi!', 'Mike', 'Nancy')
 
-# y = [1, 2, 3]
-# r = test_func(100, y)
-# print(r)
-#
-# y = [1, 2, 3]
-# r = test_func(200, y)
-# print(r)
-
-r = test_func(100)
-print(r)
-
-# リストは参照渡しなので意図しない動きをする
-# 暗黙のルールで、デフォルト引数にリストやディクショナリをするなというのがある
-r = test_func(100)
-print(r)
-
-
-# 通常はこういう書き方をする方が好ましい
-def test_func2(x, l=None):
-    if l is None:
-        l = []
-    l.append(x)
-    return l
-
-r = test_func2(100)
-print(r)
-
-r = test_func2(100)
-print(r)
+t = ('Mike', 'Nancy')
+say_something('Hi!', *t)
