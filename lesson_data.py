79062c368e960c6b5d00d78f2895d1f8191b8706
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 18:28:01 +0900

    ラムダ

--- lesson_data.py
+++ lesson_data.py
@@ -1,44 +1,18 @@
-def print_more(func):
-    def wrapper(*args, **kwargs):
-        print('func:', func.__name__)
-        print('args:', args)
-        print('kwargs:', kwargs)
-        result = func(*args, **kwargs)
-        print('result:', result)
-        return result
-    return wrapper
+l = ['Mon', 'tue', 'Wed', 'Thu', 'fri', 'sat', 'Sun']
+
+def change_words(words, func):
+    for word in words:
+        print(func(word))
 
 
-def print_info(func):
-    def wrapper(*args, **kwargs):
-        print('start')
-        result = func(*args, **kwargs)
-        print('end')
-        return result
-    return wrapper
+def sample_func(word):
+    return word.capitalize()
 
+def sample_func2(word):
+    return word.lower()
 
-@print_info
-@print_more
-def add_num(a, b):
-    return a + b
+# sample_func = lambda word: word.capitalize()
 
+change_words(l, lambda word: word.capitalize())
 
-r = add_num(10, 20)
-print(r)
-
-
-# @print_info
-# def sub_num(a, b):
-#     return a - b
-
-
-f = print_info(print_more(add_num))
-r = f(10, 20)
-print(r)
-
-# print('start')
-# r = add_num(10, 20)
-# print('end')
-#
-# print(r)
+change_words(l, lambda word: word.lower())
