2bd793c55b0f3a5db4eaa1a06a4902e161de629a
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 14:08:39 +0900

    比較演算子と論理演算子

--- lesson_data.py
+++ lesson_data.py
@@ -1,18 +1,40 @@
-x = 10
+a = 1
+b = 2
 
-if x < 0:
-    print('negative')
-elif x == 0:
-    print('zero')
-elif x == 10:
-    print('10')
-else:
-    print('positive')
+# a と b が等しい
+print(a == b)
 
-a = 5
-b = 10
+# a が b と異なる
+print(a != b)
 
+# a が b よりも小さい
+print(a < b)
+
+# a が b よりも大きい
+print(a > b)
+
+# a が b 以下である
+print(a <= b)
+
+# a が b 以上である
+print(a >= b)
+
+# a も b も真であれば真
 if a > 0:
-    print('a is positive')
     if b > 0:
-        print('b is positive')
+        print('a and b are positive')
+
+if a > 0 and b > 0:
+    print('a and b are positive')
+
+# a または b が真であれば真
+a = -1
+b = 1
+if a > 0:
+    print('a or b is positive')
+elif b > 0:
+    print('a or b is positive')
+
+if a > 0 or b > 0:
+    print('a or b is positive')
+
