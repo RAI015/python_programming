30aa0ec8a0f037ebde25f2fd369ddaeec8e1c363
RAI015 <i.scryed72342019@gmail.com>
2020-06-23 15:39:01 +0900

    enumerate関数

--- lesson_data.py
+++ lesson_data.py
@@ -1,10 +1,7 @@
-# num_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# i = 0
+# for fruit in ['apple', 'banana', 'orange']:
+#     print(i, fruit)
+#     i += 1
 
-# for i in num_list:
-#     print(i)
-
-# for i in range(2, 10, 3):
-#     print(i)
-
-for _ in range(10):
-    print('hello')
+for i, fruit in enumerate(['apple', 'banana', 'orange']):
+    print(i, fruit)
