93157db43431d94b0f493b1f6860e38800b803f7
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 19:10:37 +0900

    ファイルの読み込み

--- lesson_file.py
+++ lesson_file.py
@@ -1,3 +1,20 @@
-with open('test.txt', 'w') as f:
-    f.write('Test\n')
-    # withステートメントで書くとcloseを書くのを省略できる
+s ="""\
+AAA
+BBB
+CCC
+DDD
+"""
+
+# with open('test.txt', 'w') as f:
+#     f.write(s)
+
+with open('test.txt', 'r') as f:
+    # print(f.read())
+    while True:
+        chunk = 2
+        line = f.readline(chunk)
+        # print(line, end='')
+
+        print(line)
+        if not line:
+            break
