7f6d3325a594b79bed6e39d60f7b7846f65b6b55
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 18:20:46 +0900

    クラスの多重継承

--- lesson_data.py
+++ lesson_data.py
@@ -1,71 +1,19 @@
-import abc
+class Person(object):
+    def talk(self):
+        print('talk')
 
-class Person(metaclass=abc.ABCMeta):
-    def __init__(self, age=1):
-        self.age = age
-
-    @abc.abstractmethod
-    def drive(self):
-        pass
-
-class Baby(Person):
-    def __init__(self, age=1):
-        if age < 18:
-            super().__init__(age)
-        else:
-            raise ValueError
-
-    def drive(self):
-        raise Exception('No drive')
-
-class Adult(Person):
-    def __init__(self, age=18):
-        if age >= 18:
-            super().__init__(age)
-        else:
-            raise ValueError
-
-    def drive(self):
-        print('ok')
-
-baby = Baby()
-# baby.drive()
-adult = Adult()
-# adult.drive()
+    def run(self):
+        print('person run')
 
 class Car(object):
-    def __init__(self, model=None):
-        self.model = model
-
     def run(self):
-        print('run')
+        print('car run')
 
-    def ride(self, person):
-        person.drive()
+class PersonCarRobot(Car, Person):
+    def fly(self):
+        print('fly')
 
-
-class ToyotaCar(Car):
-    def run(self):
-        print('fast')
-
-class TeslaCar(Car):
-    def __init__(self, model='Model S',
-                 enable_auto_run=False):
-        super().__init__(model)
-        self.__enable_auto_run = enable_auto_run
-
-    @property
-    def enable_auto_run(self):
-        return self._enable_auto_run
-
-    @enable_auto_run.setter
-    def enable_auto_run(self, is_enable):
-        self._enable_auto_run = is_enable
-
-    def run(self):
-        print(self._enable_auto_run)
-        print('super fast')
-    def auto_run(self):
-        print('auto run')
-
-
+person_car_robot = PersonCarRobot()
+person_car_robot.talk()
+person_car_robot.run()
+person_car_robot.fly()
