8885afc150e4c7d4fba64e5a1ef6e41a3e9539fb
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 17:51:47 +0900

    クラスを構造体として扱う時の注意点

--- lesson_data.py
+++ lesson_data.py
@@ -11,12 +11,9 @@ class ToyotaCar(Car):
 
 class TeslaCar(Car):
     def __init__(self, model='Model S',
-                 enable_auto_run=False,
-                 passwd='123'):
-        # self.model = model
+                 enable_auto_run=False):
         super().__init__(model)
         self.__enable_auto_run = enable_auto_run
-        self.passwd = passwd
 
     @property
     def enable_auto_run(self):
@@ -24,20 +21,26 @@ class TeslaCar(Car):
 
     @enable_auto_run.setter
     def enable_auto_run(self, is_enable):
-        if self.passwd == '456':
-            self._enable_auto_run = is_enable
-        else:
-            raise  ValueError
+        self._enable_auto_run = is_enable
 
     def run(self):
-        print(self.__enable_auto_run)
+        print(self._enable_auto_run)
         print('super fast')
     def auto_run(self):
         print('auto run')
 
 
 
-tesla_car = TeslaCar('Model S', passwd='234')
-tesla_car.run()
+tesla_car = TeslaCar('Model S')
+# tesla_car.__enable_auto_run = 'XXXXXXXXXX'
 print(tesla_car.__enable_auto_run)
 
+
+class T(object):
+    pass
+
+t = T()
+t.name = 'Mike'
+t.age = 20
+
+print(t.name, t.age)
