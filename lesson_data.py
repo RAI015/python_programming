4bbef02b64da4fcdda14d88de590e0f30c7a86a6
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 17:19:43 +0900

    クラスの初期化とクラス変数

--- lesson_data.py
+++ lesson_data.py
@@ -1,8 +1,15 @@
 class Person(object):
+    def __init__(self, name):
+        self.name = name
+
     def say_something(self):
-        print('hello')
+        print('I am {}. hello'.format(self.name))
+        self.run(10)
+
+    def run(self, num):
+        print('run' * num)
 
 
-person = Person()
+person = Person('Mike')
 person.say_something()
 
