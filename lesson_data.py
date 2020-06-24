8b5ec7479b95ee11594f90e7e7f31888d25df6f9
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 15:29:36 +0900

    サードパーティライブラリ

--- lesson_data.py
+++ lesson_data.py
@@ -1,24 +1,7 @@
-s = "sssafaewfafaefeaufjadfajfeoaf"
+from termcolor import colored
 
-d = {}
-for c in s:
-    if c not in d:
-        d[c] = 0
-    d[c] += 1
-print(d)
+print('test')
 
-d = {}
-for c in s:
-    d.setdefault(c, 0)
-    d[c] += 1
-print(d)
+print(colored('test', 'green'))
 
-from collections import defaultdict
-
-d = defaultdict(int)
-
-for c in s:
-    d[c] += 1
-print(d)
-
-print(d['f'])
+# print(help(colored))
