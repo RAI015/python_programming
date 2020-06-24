e03aae262107fd7b3f69d114343ef67e6919228b
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 17:10:27 +0900

    __name__と__main__

--- lesson_data.py
+++ lesson_data.py
@@ -1,20 +1,11 @@
-# Python標準ライブラリ
-import collections
-import os
-import sys
+import lesson_package.talk.animal
 
-# サードパーティライブラリ
-import termcolor
-
-# 同じ会社の別のチームが作ったライブラリ
-import lesson_package
-
-# ローカルファイル
 import config
 
-print(collections.__file__)
-print(termcolor.__file__)
-print(lesson_package.__file__)
-print(config.__file__)
 
-print(sys.path)
+def main():
+    lesson_package.talk.animal.sing()
+
+
+if __name__ == '__main__':
+    main()
