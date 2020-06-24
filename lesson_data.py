656744693e34bba639f32697505eb64ac949e353
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 14:53:29 +0900

    ImportErrorの使い所

--- lesson_data.py
+++ lesson_data.py
@@ -1,15 +1,22 @@
 # from lesson_package.talk import human
 # from lesson_package.talk import animal
-from lesson_package.talk import *
+# from lesson_package.talk import *
 
 # import *の書き方もあまり好ましくない（何が読み込まれるかすぐにわからないため）
 
-print(animal.sing())
-print(animal.cry())
+# print(animal.sing())
+# print(animal.cry())
 
 # 関数から読み込むのではなく、モジュールから読み込むのが良い
 # r = utils.say_twice('hello')
 # print(r)
 
-print(human.sing())
-print(human.cry())
+# print(human.sing())
+# print(human.cry())
+
+try:
+    from lesson_package import utils
+except ImportError:
+    from lesson_package.tools import utils
+
+utils.say_twice('word')
