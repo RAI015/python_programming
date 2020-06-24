91ebb3636bd8691e95ff3d6ab6b35990d85c1d21
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 18:40:45 +0900

    クラスの特殊メソッド

--- lesson_data.py
+++ lesson_data.py
@@ -1,28 +1,22 @@
-def about(year):
-    print('about human {}'.format(year))
+class Word(object):
+
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return 'Word!!!!!!!!!'
+
+    def __len__(self):
+        return len(self.text)
+
+    def __add__(self, word):
+        return self.text.lower() + word.text.lower()
+
+    def __eq__(self, word):
+        return self.text.lower() == word.text.lower()
 
 
-class Person(object):
+w = Word('test')
+w2 = Word('test')
 
-    kind = 'human'
-    def __init__(self):
-        self.x = 100
-
-    @classmethod
-    def what_is_your_kind(cls):
-        return cls.kind
-
-    @staticmethod
-    def about(year):
-        print('about human {}'.format(year))
-
-
-a = Person()
-print(a.what_is_your_kind())
-
-print(Person.kind)
-print(Person.what_is_your_kind())
-
-Person.about(1999)
-
-about(1898)
+print(w)
