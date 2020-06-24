d130fab2b387439d0d225f877edfd75b31e87fb5
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 14:45:57 +0900

    アスタリフトのimportと__init__.pyと__all__の意味

--- lesson_data.py
+++ lesson_data.py
@@ -1,4 +1,11 @@
-from lesson_package.talk import human
+# from lesson_package.talk import human
+# from lesson_package.talk import animal
+from lesson_package.talk import *
+
+# import *の書き方もあまり好ましくない（何が読み込まれるかすぐにわからないため）
+
+print(animal.sing())
+print(animal.cry())
 
 # 関数から読み込むのではなく、モジュールから読み込むのが良い
 # r = utils.say_twice('hello')
---
+++ lesson_package/talk/animal.py
@@ -0,0 +1,7 @@
+from lesson_package.tools import utils
+
+def sing():
+    return '##fagagoajgaogjaogjaosing'
+
+def cry():
+    return utils.say_twice('gageajfajofajdcry')
