ebd9e22548c748803887eb7784c650a8537b83dd
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 17:32:42 +0900

    メソッドのオーバーライドとsuperによる親のメソッドの呼び出し

--- lesson_data.py
+++ lesson_data.py
@@ -1,11 +1,22 @@
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
+
     def run(self):
         print('run')
 
 class ToyotaCar(Car):
-    pass
+    def run(self):
+        print('fast')
 
 class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False):
+        # self.model = model
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+
+    def run(self):
+        print('super fast')
     def auto_run(self):
         print('auto run')
 
@@ -13,11 +24,13 @@ car = Car()
 car.run()
 
 print('#############')
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar('Lexus')
+print(toyota_car.model)
 toyota_car.run()
 # toyota_car.auto_run()
 
 print('#############')
-tesla_car = TeslaCar()
+tesla_car = TeslaCar('Model S')
+print(tesla_car.model)
 tesla_car.run()
 tesla_car.auto_run()
