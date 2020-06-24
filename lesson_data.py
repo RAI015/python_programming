59d3842f9d30138a73bfebc0cb8b5aa98a6e195e
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 17:58:19 +0900

    ダックタイピング

--- lesson_data.py
+++ lesson_data.py
@@ -1,9 +1,42 @@
+class Person(object):
+    def __init__(self, age=1):
+        self.age = age
+
+    def drive(self):
+        if self.age >= 18:
+            print('ok')
+        else:
+            raise Exception('No drive')
+
+class Baby(Person):
+    def __init__(self, age=1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+class Adult(Person):
+    def __init__(self, age=18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+baby = Baby()
+adult = Adult()
+
 class Car(object):
     def __init__(self, model=None):
         self.model = model
 
     def run(self):
         print('run')
+
+    def ride(self, person):
+        person.drive()
+
+car = Car()
+car.ride(adult)
 
 class ToyotaCar(Car):
     def run(self):
@@ -30,17 +63,3 @@ class TeslaCar(Car):
         print('auto run')
 
 
-
-tesla_car = TeslaCar('Model S')
-# tesla_car.__enable_auto_run = 'XXXXXXXXXX'
-print(tesla_car.__enable_auto_run)
-
-
-class T(object):
-    pass
-
-t = T()
-t.name = 'Mike'
-t.age = 20
-
-print(t.name, t.age)
