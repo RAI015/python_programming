9b34f3c248263f06458d846232a7cd07b7858b9d
RAI015 <i.scryed72342019@gmail.com>
2020-06-24 14:34:07 +0900

    Import文とAS

--- lesson_data.py
+++ lesson_data.py
@@ -1,16 +1,9 @@
-class UppdercaseError(Exception):
-    pass
+# import lesson_package.utils
+from lesson_package import utils
+# from lesson_package import utils as u
+# from lesson_package.utils import say_twice
 
+# 関数から読み込むのではなく、モジュールから読み込むのが良い
+r = utils.say_twice('hello')
 
-def check():
-    words = ['APPLE', 'orange', 'banana']
-    for word in words:
-        if word.isupper():
-            raise UppdercaseError(word)
-
-
-try:
-    check()
-except UppdercaseError as ex:
-    print('This is my fault. Go next')
-
+print(r)
