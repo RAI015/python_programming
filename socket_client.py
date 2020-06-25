f0c046fd85a64a7966a19a7b3692611417963f41
RAI015 <i.scryed72342019@gmail.com>
2020-06-25 17:12:50 +0900

    socket通信

---
+++ socket_client.py
@@ -0,0 +1,13 @@
+import socket
+
+
+# TCP通信
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect(('127.0.0.1', 50007))
+#     s.sendall(b'hello')
+#     data = s.recv(1024)
+#     print(repr(data))
+
+# UDP通信
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.sendto(b'hello UDP', ('127.0.0.1', 50007))
